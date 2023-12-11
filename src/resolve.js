c2af977890705cd75f2ff8170cab0bdad8e5e18a
Vladimír Gorej <vladimir.gorej@gmail.com>
2023-12-11 13:59:46 +0100

    feat: add support for resolving path templates (#9)

---
+++ src/resolve.js
@@ -0,0 +1,43 @@
+import test from './test.js'
+import parse from './parse/index.js';
+
+const isEncoded = (value) => {
+  try {
+    return typeof value === 'string' && decodeURIComponent(value) !== value;
+  } catch (e) {
+    return false;
+  }
+}
+
+const encodePathComponent = (component) => {
+  if (isEncoded(component)) {
+    return component;
+  }
+
+  return encodeURIComponent(component)
+}
+
+const resolve = (pathTemplate, parameters = {}) => {
+  if (!test(pathTemplate)) return pathTemplate;
+
+  const parseResult = parse(pathTemplate);
+  const parts = [];
+  parseResult.ast.translate(parts);
+  const resolvedPats = parts
+    .filter(([type]) => ['slash', 'path-literal', 'template-expression'].includes(type))
+    .map(([type, value]) => {
+      if (type === 'template-expression') {
+        const parameterName = value.slice(1, -1); // remove braces
+
+        return Object.hasOwn(parameters, parameterName)
+          ? encodePathComponent(parameters[parameterName])
+          : value;
+      }
+
+      return value;
+    });
+
+  return resolvedPats.join('');
+};
+
+export default resolve;
